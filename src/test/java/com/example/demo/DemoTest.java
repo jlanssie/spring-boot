3e02b585e5967215a7ad990d25d2c69f9c9ed8f3
Jeremy Lanssiers <jeremylanssiers@Jeremys-MacBook-Air.local>
2025-11-26 21:48:17 +0100

    Unit tests for Demo

---
+++ src/test/java/com/example/demo/DemoTest.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class DemoTest {
+
+    @Autowired
+    private JacksonTester<Demo> json;
+
+    @Autowired
+    private JacksonTester<Demo[]> jsonList;
+
+    private Demo[] demos;
+
+    @BeforeEach
+    void setUp() {
+        demos = Arrays.array(
+                new Demo(99L, 123.45),
+                new Demo(100L, 1.00),
+                new Demo(101L, 150.00));
+    }
+
+    @Test
+    void demoSerializationTest() throws IOException {
+        Demo demo = demos[0];
+        assertThat(json.write(demo)).isStrictlyEqualToJson("demo.json");
+        assertThat(json.write(demo)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(demo)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        assertThat(json.write(demo)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(demo)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+
+    @Test
+    void demoDeserializationTest() throws IOException {
+        String expected = """
+                {
+                    "id": 99,
+                    "amount": 123.45
+                }
+                """;
+        assertThat(json.parse(expected)).isEqualTo(new Demo(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
+
+    @Test
+    void demoListSerializationTest() throws IOException {
+        assertThat(jsonList.write(demos)).isStrictlyEqualToJson("demos.json");
+    }
+
+    @Test
+    void demoListDeserializationTest() throws IOException {
+        String expected="""
+         [
+            { "id": 99, "amount": 123.45 },
+            { "id": 100, "amount": 1.00 },
+            { "id": 101, "amount": 150.00 }
+         ]
+         """;
+        assertThat(jsonList.parse(expected)).isEqualTo(demos);
+    }
+}
