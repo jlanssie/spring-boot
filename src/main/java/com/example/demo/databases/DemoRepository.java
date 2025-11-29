758f1d1194eabe8c43c39166246e4150365c8e7a
Jeremy Lanssiers <jeremylanssiers@Jeremys-MacBook-Air.local>
2025-11-29 20:29:58 +0100

    Removed useless PagingAndSortingRepository import

--- src/main/java/com/example/demo/databases/DemoRepository.java
+++ src/main/java/com/example/demo/databases/DemoRepository.java
@@ -4,9 +4,8 @@ import com.example.demo.data.Demo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface DemoRepository extends CrudRepository<Demo, Long>, PagingAndSortingRepository<Demo, Long> {
+public interface DemoRepository extends CrudRepository<Demo, Long>{
     boolean existsByIdAndOwner(Long id, String owner);
 
     Demo findByIdAndOwner(Long id, String owner);
